1f203b80167686b899572850bf9d70cfc0bacfa2
Brian Salemi <briandsalemi@gmail.com>
2020-01-23 17:17:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(string)
   arr_of_string = string.split(" ")
-  arr_of_string.map do |word|
+  arr_of_string.collect do |word|
     word.reverse!
   end
   arr_of_string.join(" ")
